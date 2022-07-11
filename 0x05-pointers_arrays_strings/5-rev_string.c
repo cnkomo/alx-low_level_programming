050401c5c38fb61bdbb9e1784f590926c1e85885
cnkomo <58587646+cnkomo@users.noreply.github.com>
2022-07-11 22:54:58 +0200

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,3 +16,29 @@ for (i = 0; s[i]; i++)
 {
 length++;
 }
+
+count = length;
+
+star_c = s;
+end_c = s;
+
+
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
+
+
+for (i = 0; i < count / 2; i++)
+{
+
+
+c = *end_c;
+*end_c = *star_c;
+*star_c = c;
+
+
+star_c++;
+end_c--;
+}
+}
