79dfc726dc2ac8f349b0ef1e8cb97fc4e8d6018d
cnkomo <58587646+cnkomo@users.noreply.github.com>
2022-12-13 23:47:22 +0200

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+ /**
+ * main-finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Description: computes and prints even numbers < 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+unsigned long int j, k, next, sum;
+
+j = 1;
+k = 2;
+sum = 0;
+
+for (i = 1; i <= 33; ++i)
+{
+if (j < 4000000 && (j % 2) == 0)
+{
+sum = sum + j;
+}
+next = j + k;
+j = k;
+k = next;
+}
+
+printf("%lu\n", sum);
+
+return (0);
+}
