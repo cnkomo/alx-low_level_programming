ec36ebd8e73d0090976b0fc48462ba9c197cd717
cnkomo <58587646+cnkomo@users.noreply.github.com>
2022-12-14 00:04:54 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
+#ifndef  MAIN_H
+#define  MAIN_H
+
+int _putchar (char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,4 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif
+void print_times_table(int n);
+
+#endif	/*  MAIN_H *
