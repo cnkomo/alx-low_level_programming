387f678d6727a4fce174ce93fb22074ba5346121
cnkomo <58587646+cnkomo@users.noreply.github.com>
2023-01-09 23:20:07 +0200

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *main - the entry point
- *@argc: argument counter
- *@argv: array of pointer to strings
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to strings
  *
- *Return: 0
+ * Return: 0
  */
+
 
 int main(int argc, char *argv[])
 {
-	int i;
+int i;
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
