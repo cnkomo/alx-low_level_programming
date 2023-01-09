9063688d3d18ddcccfb7b7707db1ead3dc993002
cnkomo <ctnkomo@gmail.com>
2023-01-09 15:06:58 -0800

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper-evaluate if letter is uppercase
+ *@c: print int
+ * Return: 0 if successful
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
