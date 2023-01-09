d2fe683896d6c1a0c76e5559a7dc1fe7a8a433e7
cnkomo <58587646+cnkomo@users.noreply.github.com>
2023-01-09 23:22:28 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,10 +8,10 @@
  * Return: 0
  */
 
-  
-  int main(int argc, char *argv[] __attribute__ ((unused)))
 
- {
-	 printf("%d\n", argc -1);
-	 return (0);
+int main(int argc, char *argv[] __attribute__ ((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
 }
